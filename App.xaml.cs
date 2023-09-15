7aa82fb2835c241bc36d39343815cab2bb5db79d
watenk <ivar.nuij@gmail.com>
2023-09-15 19:06:13 +0200

    Added MAUI Project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace App_Dev_VisalStudio
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace App_Dev_VisalStudio
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
